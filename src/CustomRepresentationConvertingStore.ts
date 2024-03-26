75ac8c8112ae2a2240fb77f597f14a82110460b7
Lasse Fjellager <lassefj0@gmail.com>
2024-03-26 14:43:46 +0100

    added non work diffs, and a bit of custom vocab

--- src/CustomRepresentationConvertingStore.ts
+++ src/CustomRepresentationConvertingStore.ts
@@ -5,12 +5,12 @@ import {
     RepresentationConvertingStore,
     RepresentationPreferences,
     ResourceStore,
+    cloneRepresentation,
 } from "@solid/community-server";
 
 import { finished } from "stream";
 import { promisify } from "util";
 import type { Readable } from "node:stream";
-import cloneable from "cloneable-readable";
 
 import type { ResourceIdentifier } from "@solid/community-server/dist/http/representation/ResourceIdentifier";
 import type { Conditions } from "@solid/community-server/dist/storage/conditions/Conditions";
@@ -74,11 +74,20 @@ export class CustomRepresentationConvertingStore<
                 conditions
             );
 
-            diffStreams(representation.data, currentRepresentation.data).then(
-                (diff) => {
-                    this.logger.info(`We just calculated the diff: ${diff}!`);
-                }
-            );
+            let currentRep = await cloneRepresentation(currentRepresentation);
+            let newRep = await cloneRepresentation(representation);
+
+            let diff = diffStreams(currentRep, newRep);
+
+            if (diff.length === 0) {
+                this.logger.info(
+                    "The new representation is the same as the old one!"
+                );
+            }else{
+                this.logger.info(
+                    `length: ${diff.length} diff: ${diff}`
+                );
+            }
 
             const newIdentifier = {
                 path:
@@ -106,40 +115,31 @@ interface Diff {
     s2: number;
 }
 
-async function diffStreams(old: Readable, newdata: Readable): Promise<Diff[]> {
+function diffStreams(old: Representation, newdata: Representation): Diff[] {
     let diff: Diff[] = [];
     let position = 0;
 
-    const cloneS1 = cloneable(old);
-    const cloneS2 = cloneable(newdata);
+    const oldStream = old.data;
+    const newStream = newdata.data;
 
-    return new Promise((resolve, reject) => {
-        cloneS1.on("data", (chunk1: Buffer) => {
-            cloneS2.on("readable", function check() {
-                const chunk2: Buffer | null = cloneS2.read(chunk1.length);
+    console.log("HERE");
 
-                if (chunk2 === null) {
-                    cloneS2.once("readable", check);
-                } else {
-                    for (let i = 0; i < chunk1.length; i++) {
-                        if (chunk1[i] !== chunk2[i]) {
-                            diff.push({
-                                position: position + i,
-                                s1: chunk1[i],
-                                s2: chunk2[i],
-                            });
-                        }
-                    }
-                    position += chunk1.length;
-                }
-            });
+    oldStream.on("data", (chunk_old) => {
+        
+        console.log("HERE2");
+        newStream.on("data", (chunk_new) => {
+            console.log("HERE3");
+            if (chunk_old != chunk_new) {
+                console.log("data: ", chunk_old, chunk_new);
+                diff.push({
+                    position: position,
+                    s1: chunk_old.length,
+                    s2: chunk_new.length,
+                });
+            }
+            position += chunk_old.length;
         });
-
-        Promise.all([
-            promisify(finished)(cloneS1),
-            promisify(finished)(cloneS2),
-        ]).then(() => {
-            resolve(diff);
-        }, reject);
     });
+
+    return diff;
 }
---
+++ src/CustomVSolidRDF.ts
@@ -0,0 +1,8 @@
+import { createVocabulary } from "@solid/community-server";
+
+export const CustomVSolidRDF = createVocabulary(
+    'http://rdf-historcal-extension.com/vocab#',
+    'øø',
+    'ææ',
+    'åå',
+)
